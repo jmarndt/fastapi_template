eee766efbf02b598a31e57e7b40c1f2d157bb13e
Joe Arndt <jmarndt@users.noreply.github.com>
2022-11-15 16:29:31 -0600

    fixed run script and removed dependency file

--- run_api.py
+++ run_api.py
@@ -2,32 +2,39 @@ import uvicorn
 import argparse
 
 
+RUN_API = "api.main:api"
 DEFAULT_DEV_PORT = 5001
 DEFAULT_PROD_PORT = 8181
+DEFAULT_PROD_HOST = "0.0.0.0"
 
 
 parser = argparse.ArgumentParser(
     prog = 'run_api.py',
     description = 'Runs the Fast API for different environments',
-    epilog=f'If no options provided API will run in dev mode on port {DEFAULT_DEV_PORT}'
+    epilog=f'If no options provided API will run in development mode on port {DEFAULT_DEV_PORT}'
 )
-parser.add_argument('-p', '--port', help='api port', type=int, dest="port")
-parser.add_argument('--prod', '--production', help='runs api in production mode', action='store_true', dest="production")
+parser.add_argument('--port', help='port number to use', type=int, dest="port")
+parser.add_argument('--host', help='host address to use (only used in production mode)', type=str, dest="host")
+parser.add_argument('--prod', help='runs api in production mode', action='store_true', dest="production")
 
 
 def run_dev(use_port):
     if use_port == None:
         use_port = DEFAULT_DEV_PORT
-    uvicorn.run("api.main:api", port=use_port, reload=True, log_level="debug")
+    uvicorn.run(RUN_API, port=use_port, reload=True, log_level="debug")
 
 
-def run_prod(use_port):
+def run_prod(use_host, use_port):
     if use_port == None:
         use_port = DEFAULT_PROD_PORT
-    uvicorn.run("api.main:api", port=use_port)
+    if use_host == None:
+        use_host = DEFAULT_PROD_HOST
+    uvicorn.run(RUN_API, host=use_host, port=use_port)
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-
-    run_prod(args.port) if args.production else run_dev(args.port)
+    if args.production:
+        run_prod(args.host, args.port)
+    else:
+        run_dev(args.port)
